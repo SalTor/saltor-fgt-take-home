3dad506cb7d835da49021f8db32518ed8bddded0
Salvatore Torcivia <salvatoretorcivia@salvatores-mbp.lan>
2023-02-14 22:40:11 -0500

    spike test

--- src/components/shopping-cart.test.tsx
+++ src/components/shopping-cart.test.tsx
@@ -6,3 +6,11 @@ import ShoppingCart from "./shopping-cart";
 test("should show shopping cart", () => {
   render(<ShoppingCart />);
 });
+
+describe("shopping cart reflects zustand store", () => {
+  test.todo("adding item");
+  test.todo("remove item");
+  describe("recommendations", () => {
+    test.todo("shows when appropriate");
+  });
+});
